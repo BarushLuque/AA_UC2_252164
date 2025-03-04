f73b97c18ad3937096e6cd5a74ee0bd1ccca7e8b
zBook <zBook@DESKTOP-1UQ1LBV>
2025-03-04 14:16:34 -0600

    Nuevo repositorio junto con avances, clase burbuja junto con un array y un metodo de impresion de arrays

---
+++ src/main/java/com/ordenamiento/ordenamientos/Burbuja.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ordenamiento.ordenamientos;
+
+/**
+ *
+ * @author zBook
+ */
+public class Burbuja {
+    //Metodo burbuja
+    public static void burbuja(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar elementos
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
